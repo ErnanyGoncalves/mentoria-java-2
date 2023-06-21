c685d6821b5cc68942411ca80f7d96cda654c313
Túlio Henrique Santos Mendes <tulio.mendes@sensedia.com>
2023-06-21 16:49:36 -0300

    teste unitario para o webclient

--- src/test/java/com/sensedia/mentoria/factory/service/WebClientServiceImplTest.java
+++ src/test/java/com/sensedia/mentoria/factory/service/WebClientServiceImplTest.java
@@ -3,67 +3,71 @@ package com.sensedia.mentoria.factory.service;
 import com.sensedia.mentoria.factory.dto.Area;
 import com.sensedia.mentoria.factory.dto.Square;
 import com.sensedia.mentoria.factory.request.CalculateAreaRequest;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentMatchers;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockserver.integration.ClientAndServer;
-import org.mockserver.model.HttpRequest;
-import org.mockserver.model.HttpResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.reactive.ClientHttpRequest;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.web.reactive.server.WebTestClient;
-import org.springframework.util.MultiValueMap;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.BodyInserter;
-import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static reactor.core.publisher.Mono.when;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 public class WebClientServiceImplTest {
 
-    @InjectMocks
+    @Mock
     WebClient webClient;
 
-    @Autowired
-    @Value("${url.post.shape-area}")
-    private String apiUrl;
+    @Mock
+    private WebClient.RequestBodyUriSpec requestBodyUriSpecMock;
+
+    @Mock
+    private WebClient.RequestBodySpec requestBodySpecMock;
+
+    @Mock
+    private WebClient.ResponseSpec responseSpecMock;
+
+    @Mock
+    private WebClient.RequestHeadersSpec requestHeadersSpecMock;
+
+    @Mock
+    Mono<Area> areaMock = Mockito.mock(Mono.class);
+
+    @InjectMocks
+    private WebClientServiceImpl webClientService;
 
 
     @Test
     public void testPostRequest() {
+        ReflectionTestUtils.setField(webClientService, "apiUrl", "http://localhost:8080");
         Square geomShape = new Square();
         geomShape.setSide(5);
         CalculateAreaRequest reqBody = new CalculateAreaRequest(geomShape);
+        Area a = new Area(5);
 
-//        when(webClient
-//                .post()
-//                .uri(anyString())
-//                .contentType(any(MediaType.class))
-//                .body((BodyInserter<?, ? super ClientHttpRequest>) any(BodyInserters.class))
-//                .retrieve()
-//                .bodyToMono(Area.class)
-//                .block()).thenReturn(new Object());
+        when(webClient.post()).thenReturn(requestBodyUriSpecMock);
+        when(requestBodyUriSpecMock.uri(anyString())).thenReturn(requestBodySpecMock);
+        when(requestBodySpecMock.contentType(any())).thenReturn(requestBodySpecMock);
+        when(requestBodySpecMock.body(any())).thenReturn(requestHeadersSpecMock);
+        when(requestHeadersSpecMock.retrieve()).thenReturn(responseSpecMock);
+        when(responseSpecMock.bodyToMono(
+                ArgumentMatchers.<Class<Area>>notNull())).thenReturn(areaMock);
+        when(areaMock.block()).thenReturn(a);
+
+        Area areaResponse = webClientService.makeRequest(reqBody);
+
+        assertNotNull(areaResponse);
+        assertEquals(5, areaResponse.getArea());
 
     }
 
