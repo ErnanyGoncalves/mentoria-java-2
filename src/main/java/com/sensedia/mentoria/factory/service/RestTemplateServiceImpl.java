0dfbe7c523e84d6be7d416510f73bc1557c8414c
Ernany Gonçalves <ernany.goncalves@sensedia.com>
2023-06-22 16:16:55 -0300

    rest template test

--- src/main/java/com/sensedia/mentoria/factory/service/RestTemplateServiceImpl.java
+++ src/main/java/com/sensedia/mentoria/factory/service/RestTemplateServiceImpl.java
@@ -1,6 +1,5 @@
 package com.sensedia.mentoria.factory.service;
 import com.sensedia.mentoria.factory.dto.Area;
-import com.sensedia.mentoria.factory.dto.GeometricShapes;
 import com.sensedia.mentoria.factory.request.CalculateAreaRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
--- src/test/java/com/sensedia/mentoria/factory/service/RestTemplateServiceImplTest.java
+++ src/test/java/com/sensedia/mentoria/factory/service/RestTemplateServiceImplTest.java
@@ -4,89 +4,66 @@ import com.sensedia.mentoria.factory.dto.Area;
 import com.sensedia.mentoria.factory.dto.Square;
 import com.sensedia.mentoria.factory.request.CalculateAreaRequest;
 import org.junit.jupiter.api.Test;
-import org.mockserver.integration.ClientAndServer;
-import org.mockserver.model.HttpRequest;
-import org.mockserver.model.HttpResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.*;
-import org.springframework.test.context.TestPropertySource;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockserver.model.JsonBody.json;
-import org.springframework.web.client.RestTemplate;
-
-import java.net.URI;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.RestTemplate;
 
 
-@SpringBootTest
-@TestPropertySource(properties = {"url.post.shape-area=http://localhost:8080/shape-area"})
+
+
+@ExtendWith(MockitoExtension.class)
 public class RestTemplateServiceImplTest {
 
-    @Autowired
-    @Value("${url.post.shape-area}")
-    private String apiUrl;
+
+    @Mock
+    private RestTemplate restTemplate;
+
+    @Mock
+    private ResponseEntity<Area> responseEntity;
+
+    @InjectMocks
+    private RestTemplateServiceImpl restTemplateService;
+
 
 
     @Test
     public void testPostRequest() {
-
-        ClientAndServer mockServer = ClientAndServer.startClientAndServer(8080);
+        ReflectionTestUtils.setField(restTemplateService, "apiUrl", "http://localhost:8080");
+        // Crie um objeto CalculateAreaRequest fictício para o teste
         Square geomShape = new Square();
         geomShape.setSide(5);
         CalculateAreaRequest reqBody = new CalculateAreaRequest(geomShape);
 
-        try {
-            // Configurar a resposta do servidor simulado
-            Area expectedArea = new Area(25.0);
-            mockServer.when(
-                    HttpRequest.request("/shape-area")
-                            .withMethod("POST")
-                            .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                            .withBody(json(reqBody))
-            ).respond(
-                    HttpResponse.response()
-                            .withStatusCode(200)
-                            .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                            .withBody(json(expectedArea))
-            );
+        // Crie um objeto Area fictício para simular a resposta
+        Area expectedArea = new Area(25);
 
-            // Criar um objeto RestTemplate
-            RestTemplate restTemplate = new RestTemplate();
+        // Simule o comportamento do restTemplate.exchange()
+        when(restTemplate.exchange(
+                anyString(),
+                eq(HttpMethod.POST),
+                any(HttpEntity.class),
+                eq(Area.class)
+        )).thenReturn(responseEntity);
 
-            // Criar os cabeçalhos da requisição
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
+        // Simule o comportamento do responseEntity.getBody()
+        when(responseEntity.getBody()).thenReturn(expectedArea);
 
-            // Criar o objeto HttpEntity com os cabeçalhos e parâmetros
-            HttpEntity<CalculateAreaRequest> requestEntity = new HttpEntity<>(reqBody, headers);
+        // Faça a chamada ao método makeRequest()
+        Area resultArea = restTemplateService.makeRequest(reqBody);
 
-            // Enviar a requisição POST para o servidor simulado
-            ResponseEntity<Area> response = restTemplate.exchange(apiUrl, HttpMethod.POST, requestEntity, Area.class);
-
-            // Verificar o código de status da resposta
-            assertEquals(200, response.getStatusCodeValue());
-
-            // Verificar se a solicitação POST foi feita para a URL correta
-            URI requestUri = restTemplate.getUriTemplateHandler().expand(apiUrl);
-            String requestUrl = requestUri.toString();
-            assertEquals(requestUrl, "http://localhost:8080/shape-area"); // Verifique a URL esperada
-
-            // Verificar os cabeçalhos da solicitação
-            HttpHeaders requestHeaders = response.getHeaders();
-            assertTrue(requestHeaders.containsKey(HttpHeaders.CONTENT_TYPE));
-            assertEquals(MediaType.APPLICATION_JSON_VALUE, requestHeaders.getFirst(HttpHeaders.CONTENT_TYPE));
-
-            // Verificar o conteúdo da resposta
-            Area responseBody = response.getBody();
-            assertEquals(expectedArea, responseBody);
-        } finally {
-            // Parar o servidor simulado após o teste
-            mockServer.stop();
-        }
+        // Verifique se o resultado é o esperado
+        assertEquals(expectedArea, resultArea);
 
     }
 }
